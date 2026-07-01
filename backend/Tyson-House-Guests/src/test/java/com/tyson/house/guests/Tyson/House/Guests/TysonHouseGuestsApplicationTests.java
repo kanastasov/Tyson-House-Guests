f371df55a99f4a304f698f2c85818dbd961f2710
kirilanastasoff <kirilanastasoff@gmail.com>
2026-07-01 08:08:06 +0300

    initial backend generated

---
+++ backend/Tyson-House-Guests/src/test/java/com/tyson/house/guests/Tyson/House/Guests/TysonHouseGuestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyson.house.guests.Tyson.House.Guests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TysonHouseGuestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
